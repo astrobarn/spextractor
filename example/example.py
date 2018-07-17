b74c849acb068c956e1123f815849282a8a3aa34
Seméli Papadogiannakis <semelipap@gmail.com>
2018-07-17 12:17:29 +0200

    Adding updated example files

--- example/example.py
+++ example/example.py
@@ -4,11 +4,15 @@ Created on Thu Jun 11 13:13:56 2015
 Automated pEW and velocity extractor example.
 @author: Seméli Papadogiannakis
 """
-from __future__ import division
+from __future__ import division, print_function
 
 import matplotlib.pyplot as plt
 import spextractor
 plt.ioff()
+
+def report(magnitude, error):
+    for k in sorted(magnitude):
+        print(k, magnitude[k], '+-', error[k])
 
 # Just a test case with type Ia SN sn2006mo from the CfA sample.
 filename_Ia = 'sn2006mo/sn2006mo-20061113.21-fast.flm'
@@ -16,7 +20,11 @@ z = 0.0459
 pew, pew_err, vel, vel_err = spextractor.process_spectra(filename_Ia, z, downsampling=3, plot=True, type='Ia')
 
 # velocities are given in 10**3 km/s and pEW in Å
-print(pew, pew_err, vel, vel_err)
+print('pew (Å)')
+report(pew, pew_err)
+print('velocities (10**3 km/s)')
+report(vel, vel_err)
+print()
 
 plt.savefig('Ia_example.png')
 
@@ -24,16 +32,26 @@ plt.savefig('Ia_example.png')
 filename_Ib = 'tns_2018_aqf/tns_2018aqf_2018-04-10.0_P200_DBSP_ZTF.txt'
 z = 0.033
 pew, pew_err, vel, vel_err = spextractor.process_spectra(filename_Ib, z, downsampling=3, plot=True, type='Ib')
+
 # velocities are given in 10**3 km/s and pEW in Å
-print(pew, pew_err, vel, vel_err)
+print('pew (Å)')
+report(pew, pew_err)
+print('velocities (10**3 km/s)')
+report(vel, vel_err)
+print()
 plt.savefig('Ib_example.png')
 
 
 filename_Ic = 'sn2017ixh/2017ixh.txt'
 z = 0.011
-pew, pew_err, vel, vel_err = spextractor.process_spectra(filename_Ic, z, plot=True, type='Ic')
+pew, pew_err, vel, vel_err = spextractor.process_spectra(filename_Ic, z, downsampling=3, plot=True, type='Ic')
 # velocities are given in 10**3 km/s and pEW in Å
-print(pew, pew_err, vel, vel_err)
-plt.savefig('Ic_example.png')
+print('pew (Å)')
+report(pew, pew_err)
+print('velocities (10**3 km/s)')
+report(vel, vel_err)
+print()
+
+#plt.savefig('Ic_example.png', dpi=300)
 plt.show()
 
